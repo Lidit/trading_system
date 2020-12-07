07e92b77576c0ee5b64bdc20de6317eaf7b1fde7
Lidit <dropzonekr@gmail.com>
2020-12-07 21:04:38 +0900

    데이터 매니저 수정

--- data_manager.py
+++ data_manager.py
@@ -47,7 +47,7 @@ def load_stock_data(stock_code):
         data[f'volume_ma{window}_ratio'] = \
             (data[f'volume'] - data[f'volume_ma{window}']) \
             / data[f'volume_ma{window}']
-
+    #https://github.com/Crypto-toolbox/pandas-technical-indicators/blob/master/technical_indicators.py
     #MACD 데이터 계산
     short = 12
     long = 26
@@ -55,12 +55,11 @@ def load_stock_data(stock_code):
     data['MACD'] = np.zeros(len(data))
     data['MACD'] =  data['current'].ewm(span = short, min_periods = short, adjust=False).mean() - data['current'].ewm(span = long, min_periods = long, adjust=False).mean()
 
-    data['MACD_sign'] = np.zeros(len(data))
-    data['MACD_sign'] = data['MACD'].ewm(span=sign, min_periods=sign, adjust=False).mean()
+    # data['MACD_sign'] = np.zeros(len(data))
+    # data['MACD_sign'] = data['MACD'].ewm(span=sign, min_periods=sign, adjust=False).mean()
 
-    data['PDI'] = np.zeros(len(data))
-    data['MDI']= np.zeros(len(data))
-    
+    data = average_directional_movement_index(data)
+
     # data['MACD_osc'] = np.zeros(len(data))
     # data['MACD_osc'] = data['MACD'] - data['MACD_sign']
     #DMI 데이터 계산
@@ -181,7 +180,6 @@ def load_data(stock_code, date_from, date_to):
     
     data_path = f'{default_training_dir}/3.custom_training_{stock_code}.csv'
 
-
     training_data.set_index(training_data['date'], inplace=True)
 
     training_data = training_data[(training_data['date'] >= date_from) & (training_data['date'] <= date_to)]
@@ -191,6 +189,50 @@ def load_data(stock_code, date_from, date_to):
     training_data.to_csv(data_path, mode='w', header=False)
     return chart_data, training_data
 
+def average_directional_movement_index(df, n=14, n_ADX=14):
+    """Calculate the Average Directional Movement Index for given data.
+    
+    :param df: pandas.DataFrame
+    :param n: 
+    :param n_ADX: 
+    :return: pandas.DataFrame
+    """
+    i = 0
+    UpI = []
+    DoI = []
+    while i + 1 <= df.index[-1]:
+        UpMove = df.loc[i + 1, 'high'] - df.loc[i, 'high']
+        DoMove = df.loc[i, 'low'] - df.loc[i + 1, 'low']
+        if UpMove > DoMove and UpMove > 0:
+            UpD = UpMove
+        else:
+            UpD = 0
+        UpI.append(UpD)
+        if DoMove > UpMove and DoMove > 0:
+            DoD = DoMove
+        else:
+            DoD = 0
+        DoI.append(DoD)
+        i = i + 1
+    i = 0
+    TR_l = [0]
+    while i < df.index[-1]:
+        TR = max(df.loc[i + 1, 'high'], df.loc[i, 'current']) - min(df.loc[i + 1, 'low'], df.loc[i, 'current'])
+        TR_l.append(TR)
+        i = i + 1
+    TR_s = pd.Series(TR_l)
+    ATR = pd.Series(TR_s.ewm(span=n, min_periods=n).mean())
+    UpI = pd.Series(UpI)
+    DoI = pd.Series(DoI)
+    PosDI = pd.Series(UpI.ewm(span=n, min_periods=n).mean() / ATR, name='PosDI_' + str(n))
+    NegDI = pd.Series(DoI.ewm(span=n, min_periods=n).mean() / ATR, name='NegDI_' + str(n))
+    ADX = pd.Series((abs(PosDI - NegDI) / (PosDI + NegDI)).ewm(span=n_ADX, min_periods=n_ADX).mean(),
+                    name='ADX_' + str(n) + '_' + str(n_ADX))
+    df = df.join(PosDI)
+    df = df.join(NegDI)
+    df = df.join(ADX)
+    
+    return df
 
 if  __name__ == "__main__":
-    load_data('006400', '20201103090000', '20201202153000')
+    load_data('006400', '20201108090000', '20201207153000')
